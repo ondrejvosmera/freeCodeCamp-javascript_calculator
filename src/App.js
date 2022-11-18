58740b0c1d448fd4423634f11721cef4b8e965f3
ondrejvosmera <ondravosmera@seznam.cz>
2022-11-18 12:19:49 +0100

    calc_setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import './App.css'
+
+const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+const operations = ['/', '*', '-', '+', '='];
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='calculator'>
+      <div className='display'>
+        6969
+      </div>
+      <div className='numbers-container'>
+        {numbers.map(number => (
+          <button key={number} className={`button number ${number === 0 && 'wide'}`}>{number}</button>
+        ))}
+      </div>
+      <div className='operations-container'>
+        {operations.map(operation => (
+          <button key={operation} className='button math-operation'>{operation}</button>
+        ))}
+      </div>
+    </div>
   )
 }
 
